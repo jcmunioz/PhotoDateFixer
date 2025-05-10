5cd6303d9ae0f0c8f4506449ea890b5fdd6fe780
Juan Carlos Muñoz <jcmunioz@hotmail.com>
2025-05-10 15:43:33 -0500

    Use JFileChooser to get the directory to process
    Also use JULI logging instead of slf4j (less dependencies)

--- src/main/java/org/jcmv/photo/PhotoFixer.java
+++ src/main/java/org/jcmv/photo/PhotoFixer.java
@@ -1,5 +1,14 @@
 package org.jcmv.photo;
 
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import lombok.extern.java.Log;
+
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -8,117 +17,111 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import lombok.extern.slf4j.Slf4j;
+import static java.util.logging.Level.*;
 
-import com.drew.imaging.jpeg.JpegMetadataReader;
-import com.drew.imaging.jpeg.JpegProcessingException;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
-import com.drew.metadata.exif.ExifIFD0Directory;
-
-@Slf4j
+@Log
 public class PhotoFixer {
 
-  private static final int DATE_TIME_TAG_ID = 306;
-  private String inputDirectoryFromCmdLine = null;
+    private static final int DATE_TIME_TAG_ID = 306;
+    private String inputDirectoryFromCmdLine = null;
 
-  private File askForInputDirectory() throws IOException {
-    File dir = processCmdLine();
-    if (dir == null) {
-      // Request directory from user
-      try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-        boolean exit;
-        do {
-          System.out.print("Enter the directory containing your photos: ");
-          String inputFolder = reader.readLine();
-          if ("exit".equals(inputFolder)) {
-            log.debug("User decided to exit");
-            System.out.println("Bye");
-            exit = true;
-          } else {
-            dir = new File(inputFolder);
-            if (!dir.isDirectory()) {
-              System.out.format("Directory %s not found%n", inputFolder);
-              exit = false;
-            } else {
-              exit = true;
+    private File askForInputDirectory() throws IOException {
+        File dir = processCmdLine();
+        if (dir == null) {
+            // Request directory from user
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                boolean exit;
+                do {
+                    JFileChooser fileChooser = new JFileChooser();
+                    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                    fileChooser.showOpenDialog(null);
+
+//          System.out.print("Enter the directory containing your photos: ");
+                    dir = fileChooser.getSelectedFile();
+                    if (dir == null) {
+                        log.info("User decided to exit");
+                        System.out.println("Bye");
+                        exit = true;
+                    } else if (!dir.isDirectory()) {
+                        System.out.format("Directory %s not found%n", dir);
+                        exit = false;
+                    } else {
+                        exit = true;
+                    }
+                } while (!exit);
             }
-          }
-        } while (!exit);
-      }
-    }
-    return dir;
-  }
-
-  private File processCmdLine() {
-    File dir = null;
-    if (inputDirectoryFromCmdLine != null) {
-      dir = new File(inputDirectoryFromCmdLine);
-      if (dir.isDirectory()) {
+        }
         return dir;
-      } else {
-        System.out.format("Directory %s not found%n", inputDirectoryFromCmdLine);
-        return null;
-      }
-    }
-    return dir;
-  }
-
-  public void run() throws Exception {
-    File srcDir = askForInputDirectory();
-    if (srcDir == null) {
-      return;
     }
 
-    for (File photoFile : srcDir.listFiles((file) -> isJpegExtension(file))) {
-      log.info("Processing file {}", photoFile.getName());
-      try {
-        Metadata metadata = JpegMetadataReader.readMetadata(photoFile);
-        boolean found = false;
-        for (Directory dir : metadata.getDirectoriesOfType(ExifIFD0Directory.class)) {
-          for (Tag tag : dir.getTags()) {
-            if (tag.getTagName().startsWith("Date/Time")) {
-              Date realDate = dir.getDate(DATE_TIME_TAG_ID);
-              if (realDate != null) {
-                found = true;
-                log.info("File {} modified time: {}, real time: {}", photoFile.getName(),
-                    formatDate(new Date(photoFile.lastModified())), formatDate(realDate));
-                photoFile.setLastModified(realDate.getTime());
-                break;
-              }
+    private File processCmdLine() {
+        File dir = null;
+        if (inputDirectoryFromCmdLine != null) {
+            dir = new File(inputDirectoryFromCmdLine);
+            if (dir.isDirectory()) {
+                return dir;
+            } else {
+                System.out.format("Directory %s not found%n", inputDirectoryFromCmdLine);
+                return null;
             }
-          }
         }
-        if (!found) {
-          System.out.format("Didn't find date for file %s!!!%n", photoFile.getName());
+        return dir;
+    }
+
+    public void run() throws Exception {
+        File srcDir = askForInputDirectory();
+        if (srcDir == null) {
+            return;
         }
-      } catch (JpegProcessingException | IOException e) {
-        log.warn("Unable to process {}", photoFile.getName(), e);
-      }
+
+        for (File photoFile : srcDir.listFiles((file) -> isJpegExtension(file))) {
+            log.log(INFO, "Processing file {0}", photoFile.getName());
+            try {
+                Metadata metadata = JpegMetadataReader.readMetadata(photoFile);
+                boolean found = false;
+                for (Directory dir : metadata.getDirectoriesOfType(ExifIFD0Directory.class)) {
+                    for (Tag tag : dir.getTags()) {
+                        if (tag.getTagName().startsWith("Date/Time")) {
+                            Date realDate = dir.getDate(DATE_TIME_TAG_ID);
+                            if (realDate != null) {
+                                found = true;
+                                log.log(INFO, "File {0} modified time: {1}, real time: {2}", new Object[]{
+                                        photoFile.getName(), formatDate(new Date(photoFile.lastModified())), formatDate(realDate)});
+                                photoFile.setLastModified(realDate.getTime());
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (!found) {
+                    System.out.format("Didn't find date for file %s!!!%n", photoFile.getName());
+                }
+            } catch (JpegProcessingException | IOException e) {
+                log.log(WARNING, "Unable to process " + photoFile.getName(), e);
+            }
+        }
     }
-  }
 
-  private boolean isJpegExtension(File file) {
-    String name = file.getName().toLowerCase();
-    return name.endsWith(".jpg") || name.endsWith(".jpeg");
-  }
-
-  private String formatDate(Date time) {
-    DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    return format.format(time);
-  }
-
-  public static void main(String[] args) {
-    try {
-      PhotoFixer photoFixer = new PhotoFixer();
-      if (args.length > 0) {
-        photoFixer.inputDirectoryFromCmdLine = args[0];
-      }
-      photoFixer.run();
-    } catch (Exception e) {
-      log.error("", e);
+    private boolean isJpegExtension(File file) {
+        String name = file.getName().toLowerCase();
+        return name.endsWith(".jpg") || name.endsWith(".jpeg");
     }
-  }
+
+    private String formatDate(Date time) {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(time);
+    }
+
+    public static void main(String[] args) {
+        try {
+            PhotoFixer photoFixer = new PhotoFixer();
+            if (args.length > 0) {
+                photoFixer.inputDirectoryFromCmdLine = args[0];
+            }
+            photoFixer.run();
+        } catch (Exception e) {
+            log.log(SEVERE, "", e);
+        }
+    }
 
 }
